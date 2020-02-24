40afb79afd1df461210f68e8b66f38f92d900fc6
Maxim Kravetz <mkravetz@student.s19.be>
2020-02-24 20:20:19 +0100

    added function atoilen

---
+++ utils.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkravetz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/24 20:18:39 by mkravetz          #+#    #+#             */
+/*   Updated: 2020/02/24 20:18:50 by mkravetz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_atoilen(const char *str, size_t *len)
+{
+	int						x;
+	unsigned long long int	nb;
+	int						symb;
+
+	x = 0;
+	nb = 0;
+	symb = 1;
+	while (str[x] == '\t' || str[x] == '\v' || str[x] == '\n'
+			|| str[x] == '\r' || str[x] == '\f' || str[x] == ' ')
+		x++;
+	if (str[x] == '+' || str[x] == '-')
+	{
+		if (str[x++] == '-')
+			symb = symb * (-1);
+	}
+	while (str[x] >= '0' && str[x] <= '9')
+	{
+		nb = (nb * 10) + (str[x] - 48);
+		if (nb > LONG_MAX)
+			return ((symb + 1) / -2);
+		x++;
+		(*len)++;
+	}
+	return ((int)(nb * symb));
+}

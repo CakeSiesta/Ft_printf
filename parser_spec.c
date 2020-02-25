5ffb7c7e58ea41d75ec4a381805d36e41db02c99
Maxim Kravetz <mkravetz@student.s19.be>
2020-02-25 21:02:54 +0100

    save

--- parser_spec.c
+++ parser_spec.c
@@ -6,7 +6,7 @@
 /*   By: jherrald <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 17:28:14 by jherrald          #+#    #+#             */
-/*   Updated: 2020/02/25 20:46:32 by jherrald         ###   ########.fr       */
+/*   Updated: 2020/02/25 20:51:55 by mkravetz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	parser_spec(const char *format, size_t len, t_f *f, va_list arg)
 {
-	printf("len in parser is%zu\n", len);
-	printf("char is [%c]\n", format[len]);
+	printf("len in parser is[%zu]\n", len);
+	printf("char is [%c]\n", format[len - 1]);
 	if (format[len] == 'd' || format[len] == 'i')
 		convers_d(arg, f);
 

ca067f52a43ff83b142fbb98ff27fd1a3d0c2eb9
jacksiesta <jacqueline.herrald@gmail.com>
2020-03-04 22:41:00 +0100

    hum yes

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mkravetz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 17:02:35 by mkravetz          #+#    #+#             */
-/*   Updated: 2020/03/04 20:12:26 by mkravetz         ###   ########.fr       */
+/*   Updated: 2020/03/04 22:19:50 by jherrald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,16 @@ int			ft_printf(const char *format, ...)
 	{
 		if (format[x] == '%')
 		{
-			put.len_perc = parser(&f, &format[x + 1], arg);
+			struc_init(&f);
+			put.len_perc = parser(&f, &format[x], arg);
+//			printf(" x is %zu\n", put.len_perc);
 			if (!f.none)
 				parse_spec(&format[x + 1], &f, &put, arg);
 			x += put.len_perc + 1 - f.none;
+//			printf("precision is [%d]\n", f.precision);
+//			printf("width is [%d]\n", f.width);
+//			printf("minus is [%d]\n", f.minus);
+//			printf("zero is [%d]\n", f.zero);
 		}
 		else
 			ft_write(format[x], &put);
@@ -41,7 +47,7 @@ int			ft_printf(const char *format, ...)
 /*
 int main()
 {
-	ft_printf("oui%0*.5%non\n", -7);
+	ft_printf("oui%0d\n", 123);
 //	printf("oui%10tnon\n");
 }
 */
